85b3edecb95a44a3e1a8864cf2bed214fddd2968
abdias-alvarado <abdias.alvarado@unah.hn>
2019-04-07 21:25:41 -0600

    Agregando footer y privateroute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import PrivateRoute from './componentes/generics/privateroute/PrivateRoute';
 import logo from './logo.svg';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
@@ -14,7 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Inner-app">
-          <Login />
+          <Catalogo />
         </div>
       </div>
     );
