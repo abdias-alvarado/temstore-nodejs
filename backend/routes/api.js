a11add54bf9b6630b8954e1951091ef08ab9d10e
abdias-alvarado <abdias.alvarado@unah.hn>
2019-04-07 18:21:47 -0600

    Agregando seguridad al sistema.

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 function apiInit(db){
-  var usuariosApi = require('./api/usuarios');
+  var usuariosApi = require('./api/usuarios')(db);
   var productosApi = require('./api/productos')(db);
   var clientesApi = require('./api/clientes')(db);
   
 
-  /*function verificarLogin(req, res, next ){
+  function verificarLogin(req, res, next ){
     var isLoggedIn = req.session.logged && true;
     if(isLoggedIn){
       next();
     }else{
-      res.status(403).json({"error":"No Autorizado"});
+      res.status(403).json({"error":"Ingrese sus credenciales para acceder."});
     }
   }
-  */
+  
 
   router.use('/usuarios', usuariosApi);
-  router.use('/productos', productosApi);
-  router.use('/clientes', clientesApi);
+  router.use('/productos', verificarLogin, productosApi);
+  router.use('/clientes', verificarLogin, clientesApi);
   
   return router;
 }
---
+++ frontend/src/componentes/pages/catalogo/Catalogo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Header from '../../generics/header/Header';
+import Footer from '../../generics/footer/Footer';
+
+class Catalogo extends Component{
+  render(){
+    return (
+      <div>
+        
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
+
+export default Catalogo;
