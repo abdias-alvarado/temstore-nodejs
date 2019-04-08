11030679fcf0107d4e1b866d3dffab4e691ad51f
rocky7777777 <noe_paz7@hotmail.com>
2019-04-07 20:19:16 -0600

    agregadoscomponentesmaneraestatica

---
+++ frontend/src/ClientesCategoryRow.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class ClientesCategoryRow extends React.Component {
+  
+    constructor() {
+        super()
+
+    }
+
+    render() {
+        return (
+            <h4>ClientesCategoryRow</h4>
+        )
+    }
+
+}
