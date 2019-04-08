740b8cb78e3620e7e415f96f253ad983af734735
abdias-alvarado <abdias.alvarado@unah.hn>
2019-04-07 18:46:00 -0600

    Resolución de App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import './App.css';
+import Login from './componentes/pages/login/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Inner-app">
+          <Login />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
